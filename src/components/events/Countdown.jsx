fe3c6e09f1ef16d97fb26d7a30541954dfff9f80
jorgementx <jordims2000@gmail.com>
2024-11-03 22:47:40 +0100

    fix: ability to show numbers until 366 instead of limiting to 99

--- src/components/events/Countdown.jsx
+++ src/components/events/Countdown.jsx
@@ -2,39 +2,10 @@ import { useEffect, useState } from "react";
 import "./countdown.css";
 
 function Countdown({ eventDate}) {
-  //const initialTime = days * 86400 + hours * 3600 + mins * 60 + secs;
 
   const targetDate = new Date(eventDate).getTime();
   const [timeRemaining, setTimeRemaining] = useState(targetDate - Date.now());
-  /*const coundownInterval = setInterval(() => {
-      if (!(daysCounter || hoursCounter || minsCounter || secsCounter)) {
-        clearInterval(countdownInterval);
-        return;
-      }
 
-      if (secsCounter !== 0) {
-        setSecsCounter(secsCounter - 1);
-        return;
-      }
-
-      setSecsCounter(59);
-      if (minsCounter !== 0) {
-        setMinsCounter(minsCounter - 1);
-        return;
-      }
-
-      setMinsCounter(59);
-      if (hoursCounter !== 0) {
-        setHoursCounter(hoursCounter - 1);
-        return;
-      }
-
-      setHoursCounter(23);
-      if (daysCounter !== 0) {
-        setDaysCounter(daysCounter - 1);
-        return;
-      }
-    }, 1000);*/
 
   useEffect(() => {
     if (timeRemaining < 0) return
@@ -50,23 +21,6 @@ function Countdown({ eventDate}) {
     }, 1000)
     return () => clearInterval(countdownInterval)
   }, [targetDate]);
-
-  /*useEffect(() => {
-    if (timeRemaining <= 0) return;
-
-    const countdownInterval = setInterval(() => {
-      setTimeRemaining((prevTime) => {
-        if (prevTime <= 1) {
-          clearInterval(countdownInterval);
-          return 0;
-        }
-        return prevTime - 1;
-      });
-    }, 1000);
-
-    // Limpiamos el intervalo cuando el componente se desmonta
-    return () => clearInterval(countdownInterval);
-  }, [timeRemaining]);*/
 
   const daysCounter = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
   const hoursCounter = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
