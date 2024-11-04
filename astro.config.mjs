6d02d1c719763f8f0f126d805bf574e9eb01b394
Gaizka Zuazo Zulaika <>
2024-11-04 22:58:52 +0100

    fwat: Update dependencies in package-lock.json
    Upgraded various packages to their latest versions, including `@antfu/install-pkg` to 0.4.1 and `@astrojs/compiler` to 2.10.3. Also removed redundant licenses and reorganized dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  site: "https://astro-moon-landing.netlify.app/",
-  integrations: [tailwind(), icon({
-    iconDir: "src/assets/icons"
-  }), react()],
+  site: "https://punto-norte-website-m4axl0xms-gaizka-zuazos-projects.vercel.app/",
+  integrations: [
+    tailwind(),
+    icon({
+      iconDir: "src/assets/icons"
+    }),
+    react()
+  ],
   adapter: vercel()
 });
